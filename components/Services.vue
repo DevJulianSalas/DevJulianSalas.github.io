2c5af6179bd1e9997371f158c17fe3a132aa531e
Julian Salas <juliansalas080@gmail.com>
2019-02-11 22:06:23 -0500

    added service page

---
+++ components/Services.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <el-row
+      type="flex"
+      justify="center"
+      class="services"
+    >
+      <el-col 
+        :span="24"
+        style="text-align: center;"
+      >
+        <h2>
+          what I love to do <span style="color:#f95231">
+            .
+          </span>
+        </h2>
+      </el-col>
+      <el-col 
+        :span="16"
+        style="padding-top:5px;"
+      >
+        <p>Nunc Placerat Mi Id Nisi Interdum Mollis. Scelerisque Mattis, Leo Quam Aliquet Diam, Congue Laoreet Elit Metus Eget Diam.</p>
+      </el-col>
+    </el-row>
+    <el-row
+      type="flex"
+      justify="center"
+      style="flex-flow: row wrap; margin: 60px; padding-top:20px;"
+    >
+      <el-col
+        v-for="(item, index) in services"
+        :key="index"
+        :span="8"
+        style="text-align: center;"
+      >
+        <div>
+          <span>{{item.name}}</span>
+          <span>{{item.icon}}</span>
+        </div>
+        <p>{{item.description}}</p>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      services: [
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        },
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        },
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        },
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        },
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        },
+        {
+          name: 'Branding',
+          description: `Fringilla augue at maximus vesti Nam pulvinar vitae neque etrolling freight.`,
+          icon: 'text'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+.services {
+  display: flex;
+  flex-flow: row wrap;
+}
+h2 {
+  color: #11242c;
+  margin: 0 0 10px;
+  text-transform: none;
+  font-weight: 500;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 200%;
+  letter-spacing: -1px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,49 @@
 <template>
-  <section class="container">
-    <el-row type="flex" class="about-us">
-      <el-col :span="11">
-        <img 
-          src="../assets/image/me3.jpeg" 
-          alt="here go me"
-          class="me"
-        >
-      </el-col>
-      <el-col :span="12">
-        <h2>Hello, I'm Julian Salas</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi ipsam odio dolorum, hic beatae numquam natus animi minus quo voluptas saepe qui omnis aut eos, fugit, eaque illum illo debitis.</p>
-        <el-row type="flex">
-          <el-col :span="6">
-            <el-button 
-              plain
-              size="large"
-              style=""
-            >
-              Hire me
-            </el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button plain>
-              Download CV
-            </el-button>
-          </el-col>
-        </el-row>
-      </el-col>
-      <el-col :span="6">
-        <p>s</p>
-      </el-col>
-      
-    </el-row>
-    
-    <p>Esto es la page principal</p>
-  </section>
+  <div>
+    <section class="container">
+      <el-row type="flex" class="about-us">
+        <el-col :span="11">
+          <img 
+            src="../assets/image/me3.jpeg" 
+            alt="here go me"
+            class="me"
+          >
+        </el-col>
+        <el-col :span="12">
+          <h2>Hello, I'm Julian Salas</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi ipsam odio dolorum, hic beatae numquam natus animi minus quo voluptas saepe qui omnis aut eos, fugit, eaque illum illo debitis.</p>
+          <el-row type="flex">
+            <el-col :span="6">
+              <el-button 
+                plain
+                size="large"
+                style=""
+              >
+                Hire me
+              </el-button>
+            </el-col>
+            <el-col :span="6">
+              <el-button plain>
+                Download CV
+              </el-button>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </section>
+    <Services/>
+  </div>
 </template>
-
 <script>
-export default {}
+import Services from '../components/Services'
+export default {
+  components: {
+    Services
+  }
+}
 </script>
 
-<style scoped>
+<style>
 p {
   margin: 0 0 10px;
   text-transform: capitalize;
